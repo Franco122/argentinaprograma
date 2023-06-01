7a14f42090826f07e6b747f4ed2418bd866551a9
franc <franco@gmail.com>
2023-06-01 03:23:29 -0300

    Practico N0 finalizado

---
+++ practico0/promedio3.js
@@ -0,0 +1,17 @@
+let numero1 = 5;
+let numero2 = 7;
+let numero3 = 9;
+let promedio = (numero1 + numero2 + numero3) / 3;
+console.log(promedio) // inciso a
+
+numero1 = -2;
+numero2 = -3;
+numero3 = -8;
+promedio = (numero1 + numero2 + numero3) / 3;
+console.log(promedio); // inciso b
+
+numero1 = -1.2;
+numero2 = -5.3;
+numero3 = -3.3;
+promedio = (numero1 + numero2 + numero3) / 3;
+console.log(promedio); // inciso c
