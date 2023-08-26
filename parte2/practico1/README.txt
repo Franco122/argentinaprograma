15b2aa07bb380480ef7d6a82999c9eb0ec8918b1
franc <franco@gmail.com>
2023-08-26 01:13:07 -0300

    update v1

---
+++ parte2/practico1/README.txt
@@ -0,0 +1,2 @@
+Calculadora basica utilizando html, css y javascript falta verificar que se ingrese una 
+cantidad limitada de digitos .
--- parte2/practico1/javascript/main.js
+++ parte2/practico1/javascript/main.js
@@ -2,6 +2,7 @@ const operando1 = document.getElementById("operando1");
 const operando2 = document.getElementById("operando2");
 const operador = document.getElementById("operador");
 const boton = document.getElementById("calcular");
+const resetButton = document.getElementById("reset");
 const resultado = document.getElementById("resultado");
 
 function suma(num1, num2) {
@@ -17,32 +18,45 @@ function producto(num1, num2) {
 }
 
 function division(num1, num2) {
+    if (num2 === 0) {
+        throw new Error("No se puede dividir por cero");
+    }
     return num1 / num2;
 }
 
 boton.addEventListener("click", () => {
-    const numero1 = parseFloat(operando1.value);
-    const numero2 = parseFloat(operando2.value);
-    const operadorSeleccionado = operador.value;
+    try {
+        const numero1 = parseFloat(operando1.value);
+        const numero2 = parseFloat(operando2.value);
+        const operadorSeleccionado = operador.value;
 
-    let resultadoCalculado;
+        let resultadoCalculado;
 
-    switch (operadorSeleccionado) {
-        case "+":
-            resultadoCalculado = suma(numero1, numero2);
-            break;
-        case "-":
-            resultadoCalculado = resta(numero1, numero2);
-            break;
-        case "*":
-            resultadoCalculado = producto(numero1, numero2);
-            break;
-        case "/":
-            resultadoCalculado = division(numero1, numero2);
-            break;
-        default:
-            resultadoCalculado = "Operador no disponible";
+        switch (operadorSeleccionado) {
+            case "+":
+                resultadoCalculado = suma(numero1, numero2);
+                break;
+            case "-":
+                resultadoCalculado = resta(numero1, numero2);
+                break;
+            case "*":
+                resultadoCalculado = producto(numero1, numero2);
+                break;
+            case "/":
+                resultadoCalculado = division(numero1, numero2);
+                break;
+            default:
+                throw new Error("**Operador no disponible**");
+        }
+
+        resultado.textContent = `El resultado es: ${resultadoCalculado}`;
+    } catch (error) {
+        resultado.textContent = `Error: ${error.message}`;
     }
+});
 
-    resultado.textContent = `El resultado es: ${resultadoCalculado}`;
+resetButton.addEventListener("click", () => {
+    operando1.value = "";
+    operando2.value = "";
+    resultado.textContent = "El resultado es: ";
 });
